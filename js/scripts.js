759a67f16e400c75a93acd57b071a99c291f4af4
Zosia Samosia <Mlody@G.pl>
2023-05-16 13:10:27 +0200

    random nuber

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,29 @@ const tabRandomNumbers = [];
 const tabFinalNumbers = [];
 
 for(let i = 0; i < howManyBalls; i++){
-    console.log(i)
+    
+    const randomNumber = Math.floor(Math.random() * maxValue + 1)
+
+    tabRandomNumbers.push(randomNumber);
+
+
+    if(tabFinalNumbers.indexOf(randomNumber) === -1 ){
+        tabFinalNumbers.push(randomNumber);
+        addElemnet('div', randomNumber);
+    }
+    else {
+        i--;
+        continue
+    }
+
 }
 
+console.log(tabRandomNumbers);
+console.log(tabFinalNumbers);
+
+
+
 });
+
+
+console.log(Math.floor(Math.random() * 10 + 1))
